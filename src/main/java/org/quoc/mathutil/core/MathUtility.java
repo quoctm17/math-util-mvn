a3e7111f417c786a4dc1125495836995ccd551b1
quoctm17 <tranminhquoc0711@gmail.com>
2023-06-26 11:57:39 +0700

    update source to see green4ever

--- src/main/java/org/quoc/mathutil/core/MathUtility.java
+++ src/main/java/org/quoc/mathutil/core/MathUtility.java
@@ -17,19 +17,31 @@ public class MathUtility {
     // ko áp dụng giai thừa cho số => 20. Vì 20! vừa đủ khít kiểu long
     //                                                  18 con số 0
     // 21!: CHỬI, NÉM RA NGOẠI LỆ
+//    public static long getFactorial(int n) {
+//        if (n < 0 || n > 20)
+//            throw new IllegalArgumentException("Invalid n. n must be between 0..20. plz ");
+//        
+//        
+//        if (n == 0 || n == 1)
+//            return 1;
+//
+//        long product = 1; // Tích khởi đầu là 1, sau đó nhân dồn vào
+//        for (int i = 2; i <= n; i++)
+//            product *= i;
+//        
+//        return product;
+//
+//    }
     public static long getFactorial(int n) {
-        if (n < 0 || n > 20)
+        if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid n. n must be between 0..20. plz ");
-        
-        
-        if (n == 0 || n == 1)
-            return 1;
+        }
 
-        long product = 1; // Tích khởi đầu là 1, sau đó nhân dồn vào
-        for (int i = 2; i <= n; i++)
-            product *= i;
-        
-        return product;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * getFactorial(n-1);
 
     }
 }
